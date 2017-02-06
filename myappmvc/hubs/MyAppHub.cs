71115a9254d4e86bc8fb30c786b3dcf94462461b
mkharyal <kharyal.mayank@gmail.com>
2017-02-06 22:06:31 +0530

    Initial Commit

---
+++ myappmvc/hubs/MyAppHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace myappmvc.hubs
+{
+    public class MyAppHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ myappmvc/scripts/myAppSignalR.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var hubServer = $.connection.myAppHub.server;
+    var hubClient = $.connection.myAppHub.client;
+
+    $.connection.hub.start()
+    .done(function () {
+        console.log('Connected!');
+        hubServer.hello();
+    })
+    .fail(function (err) {
+        console.log();
+    });    
+
+    hubClient.hello = function () {
+        writeToDisplay();
+    }
+
+    var writeToDisplay = function (message) {
+        console.log("hello");
+    };
+})();
